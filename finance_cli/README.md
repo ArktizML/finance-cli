00a7868a32d530fb8457d11e935a74fd46994329
Mateusz Lewicki <arktizdigital@gmail.com>
2025-11-03 20:20:00 +0100

    Start project

---
+++ finance_cli/README.md
@@ -0,0 +1,20 @@
+# Personal Finance CLI Assistant (Python)
+
+## Cel projektu
+Prosty system śledzenia wydatków w terminalu.  
+Ćwiczenie fundamentów Pythona: input, listy, funkcje, pliki, pętle.
+
+## Funkcjonalności
+- Dodawanie wydatków (kwota, kategoria, data)
+- Wyświetlanie wydatków
+- Sumowanie
+- Zapis i odczyt z pliku
+
+## Plan rozwoju
+- Walidacja danych wejściowych
+- Statystyki miesiąca
+- Eksport do CSV
+- Wersja GUI lub web
+
+## Status
+STARTED ✅
---
+++ finance_cli/main.py
@@ -0,0 +1,53 @@
+# =============== IMPORTY ===============
+from datetime import datetime
+
+# =============== GLOBALNE DANE ===============
+wydatki = []  # lista słowników
+
+# =============== FUNKCJE PODSTAWOWE ===============
+def wczytaj_z_pliku():
+    pass  # TODO: wczytaj dane z pliku i dodaj do listy 'wydatki'
+
+def zapisz_do_pliku():
+    pass  # TODO: zapisz listę wydatków do pliku
+
+def dodaj_wydatek():
+    pass  # TODO: input użytkownika, dodaj słownik {"kwota": x, "kategoria": y, "data": dzisiaj}
+
+def pokaz_wydatki():
+    pass  # TODO: ładnie wypisz wszystkie wydatki
+
+def sumuj_wydatki():
+    pass  # TODO: policz sumę i wypisz wynik
+
+# =============== MENU ===============
+def pokaz_menu():
+    print("\n=== Personal Finance CLI ===")
+    print("1. Dodaj wydatek")
+    print("2. Wyświetl wydatki")
+    print("3. Pokaż sumę wydatków")
+    print("4. Zapisz i wyjdź")
+
+# =============== PĘTLA GŁÓWNA ===============
+def main():
+    wczytaj_z_pliku()
+
+    while True:
+        pokaz_menu()
+        wybor = input("Wybierz opcję: ")
+
+        if wybor == "1":
+            dodaj_wydatek()
+        elif wybor == "2":
+            pokaz_wydatki()
+        elif wybor == "3":
+            sumuj_wydatki()
+        elif wybor == "4":
+            zapisz_do_pliku()
+            print("Zapisano. Do zobaczenia!")
+            break
+        else:
+            print("Nieprawidłowa opcja.")
+
+if __name__ == "__main__":
+    main()
